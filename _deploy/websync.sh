a869834da803da3d43e7ac905d4fbe26f466d7d9
KonstantinosX <kostasx@cs.umd.edu>
2017-05-18 00:11:18 -0500

    pushing script fix

--- _deploy/websync.sh
+++ _deploy/websync.sh
@@ -3,6 +3,8 @@
 HTDOCS=/fs/userdata/kostasx/www
 HTREPO=$HOME/repos/homepage/_site/
 
-rsync -va $HTREPO $HTDOCS 
+rsync -va $HTREPO $HTDOCS
 find $HTDOCS -type d -exec chmod 755 {} +
 find $HTDOCS -type f -exec chmod 664 {} +
+
+#temporary fix for this script is chmod -R 755 * in the /fs/userdata/kostasx/www folder 
